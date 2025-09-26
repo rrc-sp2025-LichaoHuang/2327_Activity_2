d4b1b927cae72641c814c9a616668528e4c8e6e6
Lichao Huang <lhuang38@rrc.ca>
2025-09-26 16:44:04 -0500

    shape

--- shape/shape.py
+++ shape/shape.py
@@ -1,4 +1,53 @@
 """This module defines the Shape class."""
 
-__author__ = ""
-__version__ = ""
+__author__ = "Lichao Huang"
+__version__ = "1.0.0"
+
+from abc import ABC, abstractmethod
+
+class Shape(ABC):
+    """
+    An abstract shape class
+    
+    """
+    def __init__(self, color: str):
+        """
+
+        args:
+            color (str): The color of the shape.
+
+        Raises:
+            ValueError: color is blank.
+
+        """
+        if len(color.strip()) > 0:
+            self._color = color
+        else:
+            raise ValueError("Color cannot be blank.")
+        
+    def __str__(self) -> str:
+        """
+        return a string representation of the information of the shape.
+
+        Return:
+            str: The color of the shape.
+
+        """
+        return(f"The shape color is {self._color}.")
+    
+    @abstractmethod
+    def calculate_area(self)-> float:
+        """
+        Undefined abstract area calculator.
+
+        """
+        pass
+
+    
+    @abstractmethod
+    def calculate_perimeter(self) -> float:
+        """
+        Undefined abstract perimeter calculator.
+
+        """
+        pass
